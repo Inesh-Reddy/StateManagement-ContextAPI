83011cb9ebb992aa4e0a12c78205685e39154e5a
Inesh-Reddy <ineshreddy007@gmail.com>
2024-02-25 15:59:32 +0530

    statemanagement using ContextAPI

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useContext, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CountContext } from './context'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+        <Count count={count} setCount={setCount} />
+      </CountContext.Provider>
+    </div>
+  )
+}
+
+function Count({setCount}){
+  console.log("rerendering from Count Component")
+  return <div>
+    <CountRenderer ></CountRenderer>
+    <Buttons setCount={setCount}/>
+  </div>
+}
+
+function CountRenderer(){
+  const count =useContext(CountContext);
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}){
+  const count =useContext(CountContext);
+  return <div>
+    <button onClick={()=>{
+      setCount(count+1)
+    }} >Increase</button>
+    <button onClick={()=>{
+      setCount(count-1)
+    }} >Decrease</button>
+  </div>
+}
+
+export default App
